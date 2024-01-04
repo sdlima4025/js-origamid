0622393046284d46fd55471f0065f482987dfa64
sdlima4025 <sergiolimafg3@gmail.com>
2024-01-04 18:06:58 -0300

    revisado fetch e await

--- 06-fetch/script.js
+++ 06-fetch/script.js
@@ -1,1 +1,34 @@
+/** uma requisição fetch sempre retorna uma Promise<Response>
+ * (input: RequestInfo | URL, init?: RequestInit | undefined):
+ * Promise<Response>  */
+
 fetch("https://ranekapi.origamid.dev/json/api/produto")
+  .then(r => r.json())
+  .then((jsonBody) => {
+    document.querySelector("#app").innerHTML = jsonBody[0].nome;
+    console.log(jsonBody);
+  });
+
+  /** Enviando dados para API */
+
+  const data = {
+  id: "andrerafa",
+  nome: "Andre",
+  email: "andrerafa@origamid.com",
+  senha: "123456",
+  cep: "123456",
+  rua: "Ali Perto",
+  numero: "230",
+  bairro: "Botafogo",
+  cidade: "Rio de Janeiro",
+  estado: "Rio de Janeiro"
+  }
+  fetch("https://ranekapi.origamid.dev/json/api/usuario", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+
---
+++ 07-async-await/script.js
@@ -0,0 +1,23 @@
+// async function fetchProdutos(url) {
+//     const response = await fetch(url);
+//     const json = await response.json();
+//     return JSON
+// }
+
+
+// fetchProdutos("https://ranekapi.origamid.dev/json/api/produto")
+
+async function fetchProdutos(url) {
+    const response = await fetch(url);
+    const jsonBody = await response.json();
+    return jsonBody;
+}
+
+const requisicao = fetchProdutos (
+    "https://ranekapi.origamid.dev/json/api/produto"
+
+)
+requisicao.then(Response => {
+    console.log(Response)
+
+});
